1605da60d22a7e8a8823c06ae6435598cda73d7f
Tey - nullthefirst <usheninte234@gmail.com>
2026-04-28 08:19:46 +0100

    Added catalogue resource

--- src/catalogue/catalogue.module.ts
+++ src/catalogue/catalogue.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { CatalogueService } from './catalogue.service';
+import { CatalogueController } from './catalogue.controller';
 
-@Module({})
+@Module({
+  controllers: [CatalogueController],
+  providers: [CatalogueService],
+})
 export class CatalogueModule {}
