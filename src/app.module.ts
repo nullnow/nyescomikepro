7e7efa3b53dbc8f0c1c10947574349432ed88c42
Tey - nullthefirst <usheninte234@gmail.com>
2026-04-28 08:09:22 +0100

    Added other context directories

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { AboutModule } from './about/about.module';
+import { CatalogueModule } from './catalogue/catalogue.module';
+import { SocialsModule } from './socials/socials.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    AuthModule,
+    UsersModule,
+    AboutModule,
+    CatalogueModule,
+    SocialsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
