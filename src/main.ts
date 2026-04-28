f7fb7eccc0144079c5594f978b3cb3e6e3c80bee
Tey - nullthefirst <usheninte234@gmail.com>
2026-04-28 17:02:27 +0100

    Updated entrypoint file

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
-
 import { join } from 'path';
 import cookieParser from 'cookie-parser';
-
 import { AppModule } from './app.module';
 
 async function bootstrap() {
