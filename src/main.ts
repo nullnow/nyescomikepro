7373623cd33a6a3ba89f33d19b37758bb9db8364
Tey - nullthefirst <usheninte234@gmail.com>
2026-04-28 07:49:00 +0100

    Updated main entrypoint file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
+import { join } from 'path';
+import cookieParser from 'cookie-parser';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.use(cookieParser());
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+
+  app.setViewEngine('ejs');
+
+  await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
